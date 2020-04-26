c48659a707b3af89081357a94c39bbcaf781ebeb
Aaronapple <aaronliu.625@gmail.com>
2020-04-26 23:55:41 +0800

    week3assignment

---
+++ GameCoursera/course1/week3/assignment/RockOfAges/Assets/Scripts/RockSpawner.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockSpawner : MonoBehaviour
+{
+    // needed for spawning
+    [SerializeField]
+    GameObject prefabRock;
+
+    // saved for efficiency
+    [SerializeField]
+    Sprite rockSprite0;
+    [SerializeField]
+    Sprite rockSprite1;
+    [SerializeField]
+    Sprite rockSprite2;
+
+    public GameObject[] items;
+
+    // spawn control
+    const float SpawnDelay = 1;
+    Timer spawnTimer;
+
+    // spawn location support
+    int SpawnX;
+    int SpawnY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        SpawnX = Screen.width / 2;
+        SpawnY = Screen.height / 2;
+
+        // create and start timer
+        spawnTimer = gameObject.AddComponent<Timer>();
+        spawnTimer.Duration = SpawnDelay;
+        spawnTimer.Run();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        items = GameObject.FindGameObjectsWithTag("rockObject");        
+        if (spawnTimer.Finished)
+        {
+            spawnTimer.Run();
+            if (items.Length < 3)
+            {
+                SpawnRock();
+            }
+        }
+    }
+    
+
+    void SpawnRock()
+    {
+        // generate center location and create new rock
+        Vector3 location = new Vector3(SpawnX, SpawnY,
+                                       -Camera.main.transform.position.z);
+        Vector3 worldLocation = Camera.main.ScreenToWorldPoint(location);
+        GameObject rock = Instantiate(prefabRock) as GameObject;
+        rock.transform.position = worldLocation;
+
+        // set random sprite for new rock
+        SpriteRenderer spriteRenderer = rock.GetComponent<SpriteRenderer>();
+        int spriteNumber = Random.Range(0, 3);
+        if (spriteNumber == 0)
+        {
+            spriteRenderer.sprite = rockSprite0;
+        }
+        else if (spriteNumber == 1)
+        {
+            spriteRenderer.sprite = rockSprite1;
+        }
+        else
+        {
+            spriteRenderer.sprite = rockSprite2;
+        }
+    }
+}
---
+++ GameCoursera/course1/week3/assignment/RockOfAges/Assets/Scripts/rock.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class rock : MonoBehaviour
+{
+    float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // apply impulse force to get game object moving
+        const float MinImpulseForce = 2f;
+        const float MaxImpulseForce = 4f;
+        float angle = Random.Range(0, 2 * Mathf.PI);
+        speed = Random.Range(2,6);
+        Vector2 direction = new Vector2(
+            Mathf.Cos(angle), Mathf.Sin(angle));
+        float magnitude = Random.Range(MinImpulseForce, MaxImpulseForce);
+        GetComponent<Rigidbody2D>().AddForce(
+            direction * magnitude,
+            ForceMode2D.Impulse);
+    }
+
+    void Update()
+    {
+        this.transform.Rotate(Vector3.forward*speed);
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
